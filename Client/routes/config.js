5c2a43a054194ecd1e0fdf25134f686f4c6ae65a
ChandanaRao13 <chandana.rao13@gmail.com>
2017-01-23 01:41:22 -0800

    Amazon Fresh

---
+++ Client/routes/config.js
@@ -0,0 +1,13 @@
+
+exports.db= {
+	    "host"     : 'localhost',
+	    "user"     : 'root',
+	    "password" : 'Kashyapc',
+	    "database" : 'AMAZON',
+	    "port"	 : 3306
+	};
+
+exports.dbpool={
+		
+		"maxsize": 50
+};
